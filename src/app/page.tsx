d58ca6bfb8fd2f61c2014611c96e76b121c55af3
Edily Mora <edilyrosa@gmail.com>
2024-08-01 20:44:41 -0400

    style and zod for forms of register

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,9 @@
 "use client"
-import ContainerWeb from "@/components/ContainerWeb";
-import GridContainer from "@/components/GridContainer";
-import GridMain from "@/components/GridMain";
-import GridSecond from "@/components/GridSecond";
-import Header from "@/components/Header";
 import Bienvenida from "@/components/Bienvenida";
 
 export default function Home() {
   return (
     <>
-      <Header title={"Bienvenidos!"} />
       <Bienvenida />
     </>
   );
