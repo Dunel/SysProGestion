eaac146000176f2ec559cd9c16b59ccb0cdeb2b0
Edily Mora <edilyrosa@gmail.com>
2024-08-02 19:53:33 -0400

    listo

--- src/components/register/Step1.tsx
+++ src/components/register/Step1.tsx
@@ -1,13 +1,4 @@
-"use client";
-import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
-import { cn } from "@/components/lib/utils";
 
-//TODO: traking ESTO
- type Step1Props = {
-    setCode: Function;
-   validateCode: Function;
-  };
 "use client";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
@@ -49,28 +40,9 @@ export default function Step1({ setCode, validateCode }: Step1Props) {
   );
 }
 
-const BottomGradient = () => {
-  return (
-    <>
-      <span className="group-hover/btn:opacity-100 block transition duration-500 opacity-0 absolute h-px w-full -bottom-px inset-x-0 bg-gradient-to-r from-transparent via-cyan-500 to-transparent" />
-      <span className="group-hover/btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent via-indigo-500 to-transparent" />
-    </>
-  );
-};
 
-const LabelInputContainer = ({
-  children,
-  className,
-}: {
-  children: React.ReactNode;
-  className?: string;
-}) => {
-  return (
-    <div className={cn("flex flex-col space-y-2 w-full", className)}>
-      {children}
-    </div>
-  );
-};
+
+
 
 const BottomGradient = () => {
   return (
