06c3b523c2c3b45bc7c003ee780c954bf58d7390
Edily Mora <edilyrosa@gmail.com>
2024-08-02 19:49:54 -0400

    test of branch

--- src/components/register/Step1.tsx
+++ src/components/register/Step1.tsx
@@ -3,6 +3,7 @@ import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { cn } from "@/components/lib/utils";
 
+//TODO: traking ESTO
  type Step1Props = {
     setCode: Function;
    validateCode: Function;
