92f4dc476edbbc2edfb5b839b50b466cba12c118
Edily Mora <edilyrosa@gmail.com>
2024-08-08 12:20:17 -0400

    using flex

--- src/components/Bienvenida.jsx
+++ src/components/Bienvenida.jsx
@@ -24,7 +24,7 @@ export default function Bienvenida() {
               className="relative flex flex-col gap-4 items-center justify-center px-4 space-y-6"
             >
 
-      <div className='space-y-4 '>
+      <div className='space-y-4 w-full mx-auto p-6'>
             
             <p className="text-gray-700 text-justify text-2xl md:text-3xl lg:text-3xl leading-12 mt-40">
               Esta aplicación web está diseñada para facilitar la gestión de los procesos de pasantías y servicios comunitarios
@@ -33,65 +33,67 @@ export default function Bienvenida() {
             </p>
       </div>
        
-          <div className="w-full mx-auto p-6 rounded-lg mt-10 child w-4/5 text-xl">
+          <div className="w-full mx-auto p-6 rounded-lg mt-5 child w-4/5 text-xl">
 
             <h2 className="text-2xl font-bold text-gray-800 mb-4 text-center md:text-4xl lg:text-4xl">Roles en la Aplicación</h2>
             
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-4 h-auto">
               {/* Rol de Alcaldía */}
-              <div className="bg-blue-50 p-4 rounded-lg">
+              <div className=" flex flex-col bg-blue-50 p-4 rounded-lg h-auto">
                 <h3 className="text-xl font-bold text-blue-800 mb-2 text-center">Alcaldía (Administrador)</h3>
                 <p className="text-blue-700 mb-2 text-justify">
                   La Alcaldía es responsable de gestionar y supervisar todo el proceso de pasantías y servicios comunitarios.
                   Pueden publicar nuevas vacantes, aprobar solicitudes de estudiantes y generar informes.
                 </p>
-                <img src="https://cdn.pixabay.com/photo/2018/02/23/19/40/elegant-3176410_640.png" alt="Alcaldía" className="w-1/3 h-1/2 mx-auto" />
+                <img src="https://cdn.pixabay.com/photo/2018/02/23/19/40/elegant-3176410_640.png" alt="Alcaldía" className="w-1/3 mx-auto" />
               </div>
 
               {/* Rol de Dependencia */}
-              <div className="bg-green-50 p-4 rounded-lg ">
+              <div className="flex flex-col bg-green-50 p-4 rounded-lg h-auto">
                 <h3 className="text-xl font-bold text-green-800 mb-2 text-center">Dependencia de Alcaldía</h3>
                 <p className="text-green-700 mb-2 text-justify">
                   Las dependencias Autorizadas por la Alcaldía, pueden ofertar vacantes de pasantías
                   y servicios comunitarios. Ellos describen los requisitos, habilidades y tareas de cada vacante.
                 </p>
-                <img src="https://cdn.pixabay.com/photo/2020/12/18/00/43/business-5840870_960_720.png" alt="Dependencia" className="w-1/3 h-1/2 mx-auto" />
+                <img src="https://cdn.pixabay.com/photo/2020/12/18/00/43/business-5840870_960_720.png" alt="Dependencia" className="w-1/3 mx-auto mt-4" />
               </div>
 
               {/* Rol de Estudiante */}
-              <div className="bg-yellow-50 p-4 rounded-lg">
+              <div className="flex flex-col bg-yellow-50 p-4 rounded-lg h-auto">
                 <h3 className="text-xl font-bold text-yellow-800 mb-2 text-center">Estudiante</h3>
                 <p className="text-yellow-700 mb-2 text-justify">
                   Los estudiantes universitarios pueden buscar y aplicar a las vacantes de pasantías y servicios comunitarios
                   que mejor se adapten a su perfil y ubicación. Pueden ver el estado de sus solicitudes y recibir notificaciones.
                 </p>
-                <img src="https://cdn.pixabay.com/photo/2021/01/28/06/23/woman-5957134_1280.png" alt="Estudiante" className="w-1/3 h-1/1 mx-auto" />
+                <img src="https://cdn.pixabay.com/photo/2021/01/28/06/23/woman-5957134_1280.png" alt="Estudiante" className="w-1/3 mx-auto -m-4" />
             </div>
               </div>
 
-            <p className="text-gray-700 mb-4 text-justify text-2xl md:text-3xl lg:text-3xl">
-              Explora las diferentes secciones de la aplicación para descubrir más sobre cada rol y cómo participar en este
-              programa de pasantías y servicios comunitarios.
-            </p>
+              <div className='space-y-4 w-full mx-auto p-6'>
+                <p className="text-gray-700 text-justify text-2xl md:text-3xl lg:text-3xl leading-12">
+                  Explora las diferentes secciones de la aplicación para descubrir más sobre cada rol y cómo participar en este
+                  programa de pasantías y servicios comunitarios.
+                </p>
+              </div>
 
 
-          
-            {/* Botón de registro */}
-<div className="flex flex-col md:flex-row justify-center mb-4 text-center">
-  <div className='min-w-[50%] w-auto md:mr-4'>
-    <label className="block text-sm font-medium text-gray-700 mb-1">¿Ya tienes una cuenta?</label>
-    <button className="relative z-50 w-auto min-w-[80%] bg-gray-950 text-white py-2 px-4 rounded-lg hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500 text-xl">
-      <Link href="/login">Iniciar sesión</Link>
-    </button>
-  </div>
+            {/* Botones de inicio de sesión y registro */} 
 
-  <div className='min-w-[50%] w-auto md:ml-4 mt-4 md:mt-0'>
-    <label className="block text-sm font-medium text-gray-700 mb-1">¿No tienes una cuenta?</label>
-    <button className="relative z-50 w-auto min-w-[80%] bg-gray-950 text-white py-2 px-4 rounded-lg hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500 text-xl">
-      <Link href="/register">Regístrate</Link>
-    </button>
-  </div>
-</div>
+            <div className="flex flex-col items-stretch my-4 text-center md:flex-row">
+              <div className='w-full md:mr-4'>
+                <label className="block text-sm font-medium text-gray-700 mb-1">¿Ya tienes una cuenta?</label>
+                <button className="relative z-50 w-full bg-gray-950 text-white py-2 px-4 rounded-lg hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500 text-xl">
+                  <Link href="/login">Iniciar sesión</Link>
+                </button>
+              </div>
+
+              <div className='w-full md:ml-4 mt-4 md:mt-0'>
+                <label className="block text-sm font-medium text-gray-700 mb-1">¿No tienes una cuenta?</label>
+                <button className="relative z-50 w-full bg-gray-950 text-white py-2 px-4 rounded-lg hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500 text-xl">
+                  <Link href="/register">Regístrate</Link>
+                </button>
+              </div>
+            </div>
 
 
 
--- src/components/Title-h1.jsx
+++ src/components/Title-h1.jsx
@@ -1,27 +1,43 @@
 import { useEffect, useState } from 'react';
-export default function TitleH1({title}) {
 
+export default function TitleH1({ title }) {
   const [topPosition, setTopPosition] = useState('50%');
+  const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
+    // Cambia el estado de isMobile según el tamaño de la ventana
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768); // Considera móvil si el ancho es menor a 768px
+    };
+
+    handleResize(); // Llama a la función al montar el componente
+    window.addEventListener('resize', handleResize); // Agrega el listener para cambios de tamaño
+
     const timer = setTimeout(() => {
-      setTopPosition('7rem'); // Cambia a 7rem después de 3 segundos, importante por <AuroraBackground>
+      setTopPosition('7rem'); // Cambia a 7rem después de 10ms
     }, 10);
 
-    return () => clearTimeout(timer); // Limpia el timer si el componente se desmonta
+    return () => {
+      clearTimeout(timer); // Limpia el timer si el componente se desmonta
+      window.removeEventListener('resize', handleResize); // Limpia el listener al desmontar
+    };
   }, []);
- 
+
+  useEffect(() => {
+    setTopPosition(isMobile ? '-10%' : '50%'); // Actualiza topPosition según el estado isMobile
+  }, [isMobile]);
 
   return (
-<div className='flex items-center justify-center text-center m-2'>
+    <div className='flex items-center justify-center text-center m-2'>
       <h1
         className={`transition-all duration-1000 absolute font-bold text-gray-900 text-3xl md:text-4xl lg:text-5xl`} // Transición de 1000ms
-        style={{ top: topPosition, transform: 'translateY(-50%)' }} // Centrado verticalmente
-        >
-       {title}
+        style={{
+          top: topPosition,
+          transform: 'translateY(-50%)'
+        }}
+      >
+        {title}
       </h1>
-          </div>
-      
-   
-  )
+    </div>
+  );
 }
