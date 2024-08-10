669b81df15af8605b3a45a70ee8d5254856b4621
Daniel Amaya <daniel.amaya18@gmail.com>
2024-08-10 10:27:33 -0400

    get profile

--- src/app/api/estudiante/perfil/route.ts
+++ src/app/api/estudiante/perfil/route.ts
@@ -1,6 +1,10 @@
-import { ProfileUpdate } from "@/controllers/profile.controller";
+import { ProfileGet, ProfileUpdate } from "@/controllers/profile.controller";
 import { NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
   return await ProfileUpdate(req);
+}
+
+export async function GET(req: NextRequest) {
+  return await ProfileGet(req);
 }
--- src/validations/profile.schema.ts
+++ src/validations/profile.schema.ts
@@ -1,54 +1,3 @@
-// import { z } from "zod";
-
-// export const profileSchema = z.object({
-//   names: z
-//     .string({ required_error: "El nombre es requerido" })
-//     .min(3, { message: "El nombre debe tener minimo 3 caracteres" })
-//     .max(50, { message: "El nombre debe tener maximo 50 caracteres" })
-//     .transform((val) => val.toUpperCase()),
-//   lastnames: z
-//     .string({ required_error: "El apellido es requerido" })
-//     .min(3, { message: "El apellido debe tener minimo 3 caracteres" })
-//     .max(50, { message: "El apellido debe tener maximo 50 caracteres" })
-//     .transform((val) => val.toUpperCase()),
-//   phone: z
-//     .string({ required_error: "El telefono es requerido" })
-//     .min(10, { message: "El telefono debe tener minimo 10 caracteres" })
-//     .max(12, { message: "El telefono debe tener maximo 10 caracteres" }),
-//   address: z
-//     .string({ required_error: "La dirección es requerida" })
-//     .min(10, { message: "La dirección debe tener minimo 10 caracteres" })
-//     .max(100, { message: "La dirección debe tener maximo 100 caracteres" }),
-//   university: z
-//     .string({ required_error: "La universidad es requerida" })
-//     .min(4, { message: "La universidad debe tener minimo 10 caracteres" })
-//     .max(50, { message: "La universidad debe tener maximo 100 caracteres" }),
-//   quarter: z
-//     .string({ required_error: "El trimestre es requerido" })
-//     .min(1, { message: "El trimestre debe tener minimo 1 caracteres" })
-//     .max(2, { message: "El trimestre debe tener maximo 2 caracteres" })
-//     .transform((val) => parseInt(val))
-//     .refine((val) => val >= 1 && val <= 12, {
-//       message: "Trimestre no válido",
-//     }),
-//   skills: z
-//     .string({ required_error: "Las habilidades son requeridas" })
-//     .min(5, { message: "Las habilidades debe tener minimo 10 caracteres" })
-//     .max(100, { message: "Las habilidades debe tener maximo 100 caracteres" }),
-//   interests: z
-//     .string({ required_error: "Los intereses son requeridos" })
-//     .min(10, { message: "Los intereses debe tener minimo 10 caracteres" })
-//     .max(100, { message: "Los intereses debe tener maximo 100 caracteres" }),
-//   description: z
-//     .string({ required_error: "La descripción es requerida" })
-//     .min(7, { message: "La descripción debe tener minimo 10 caracteres" })
-//     .max(100, { message: "La descripción debe tener maximo 100 caracteres" }),
-// });
-
-
-
-
-
 import { z } from "zod";
 
 export const profileSchema = z.object({
@@ -95,7 +44,13 @@ export const profileSchema = z.object({
         return z.NEVER;
       }
       return trimestre;
-    }).or(z.number().min(1).max(12)),
+    })
+    .or(
+      z
+        .number()
+        .min(1, { message: "El trimestre debe ser mayor a 0" })
+        .max(12, { message: "El trimestre debe ser menor a 13" })
+    ),
   skills: z
     .string({ required_error: "Las habilidades son requeridas" })
     .min(5, { message: "Las habilidades debe tener minimo 5 caracteres" })
