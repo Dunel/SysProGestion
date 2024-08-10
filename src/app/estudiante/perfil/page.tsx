527b50d552a67e1d693d3b7e323323d87349c00e
Daniel Amaya <daniel.amaya18@gmail.com>
2024-08-10 10:59:07 -0400

    fix

--- src/app/estudiante/perfil/page.tsx
+++ src/app/estudiante/perfil/page.tsx
@@ -14,7 +14,6 @@ import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { cn } from "@/components/lib/utils";
 
-
 export default function Page() {
   const { data: session, update } = useSession();
   const {
