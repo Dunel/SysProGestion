387f9a3c83d1baedfe48f1a9ed8a357f9c51c97c
Edily Mora <edilyrosa@gmail.com>
2024-08-10 22:42:11 -0400

    profile student ready!

--- src/app/estudiante/perfil/page.tsx
+++ src/app/estudiante/perfil/page.tsx
@@ -1,184 +1,3 @@
-
-// "use client";
-// import ContainerWeb from "@/components/ContainerWeb";
-// import GridContainer from "@/components/GridContainer";
-// import GridMain from "@/components/GridMain";
-// import GridSecond from "@/components/GridSecond";
-// import Header from "@/components/Header";
-// import { useSession } from "next-auth/react";
-// import { useState } from "react";
-// import EstudianteProfile from "@/components/perfiles/EstudianteProfile";
-// import axios from "axios";
-
-// export default function Page() {
-//   const { data: session, update } = useSession();
-//   const [names, setNames] = useState("");
-//   const [lastnames, setLastnames] = useState("");
-//   const [phone, setPhone] = useState("");
-//   const [address, setAddress] = useState("");
-//   const [university, setUniversity] = useState("");
-//   const [quarter, setQuarter] = useState("");
-//   const [skills, setSkills] = useState("");
-//   const [interests, setInterests] = useState("");
-//   const [description, setDescription] = useState("");
-
-//   const profileUpdate = async () => {
-//     try {
-//       const res = await axios.post("/api/estudiante/perfil", {
-//         names,
-//         lastnames,
-//         phone,
-//         address,
-//         university,
-//         quarter,
-//         skills,
-//         interests,
-//         description,
-//       });
-//       if (session) {
-//         await update({ profile: true });
-//       }
-
-//       console.log(res.data);
-//       return;
-//     } catch (error) {
-//       if (axios.isAxiosError(error)) {
-//         console.log(error.response?.data.error);
-//         return;
-//       }
-
-//       console.error("error:", error);
-//     }
-//   };
-//   const profileData = {
-//     nombreEstudiante: "Jose Perez",
-//     carreraEstudiante: "Ingeniería de Software",
-//     cedulaEstudiante: "1234567890",
-//     telefonoEstudiante: "0424 7613872",
-//     correoEstudiante: "jose_p@gmail.com",
-//     domicilioEstudiante: "Calle # 123, Maracaibo, Zulia",
-//     universidadEstudiante: "Universidad del Zulia",
-//     trimestreEstudiante: "11",
-//     habilidadesEstudiante: "Programación, Matemáticas, Inglés",
-//     interesesEstudiante: "Programación, Matemáticas, Física",
-//     descripcionEstudiante:
-//       "Soy un estudiante universitario apacionado por la ciencia. Me gusta la programación y la tecnología. Mis pasatiempos son el deporte y jugar videojuegos.",
-
-//     fotoDelEstudiante:
-//       "https://lgbtravel.com/wp-content/uploads/2023/11/paises-hombres-guapos-portada-1024x576.jpg", // URL de la foto
-//   };
-//   console.log(session);
-//   return (
-//     <>
-//       <Header
-//         title={"Tu Perfil"}
-//         subtitle={
-//           "Este es tu perfil, aquí podrás visualizar tu información personal y actualizarla si es necesario."
-//         }
-//       />
-//       <ContainerWeb>
-//         <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
-//           <GridMain>
-//             <GridContainer>
-//               {session?.user.profile ? "true" : "false"}
-//               <EstudianteProfile profileData={profileData} />
-//             </GridContainer>
-//           </GridMain>
-
-//           <GridSecond>
-//             <GridContainer>
-//               <input
-//                 name="names"
-//                 value={names}
-//                 className="w-full h-10 px-3 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline"
-//                 type="text"
-//                 onChange={(e) => setNames(e.target.value)}
-//                 placeholder="Nombres"
-//               />
-//               <input
-//                 name="lastnames"
-//                 value={lastnames}
-//                 className="w-full h-10 px-3 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline"
-//                 type="text"
-//                 onChange={(e) => setLastnames(e.target.value)}
-//                 placeholder="Apellidos"
-//               />
-//               <input
-//                 name="phone"
-//                 value={phone}
-//                 className="w-full h-10 px-3 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline"
-//                 type="text"
-//                 onChange={(e) => setPhone(e.target.value)}
-//                 placeholder="Teléfono"
-//               />
-//               <input
-//                 name="address"
-//                 value={address}
-//                 className="w-full h-10 px-3 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline"
-//                 type="text"
-//                 onChange={(e) => setAddress(e.target.value)}
-//                 placeholder="Dirección"
-//               />
-//               <input
-//                 name="university"
-//                 value={university}
-//                 className="w-full h-10 px-3 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline"
-//                 type="text"
-//                 onChange={(e) => setUniversity(e.target.value)}
-//                 placeholder="Universidad"
-//               />
-//               <input
-//                 name="quarter"
-//                 value={quarter}
-//                 className="w-full h-10 px-3 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline"
-//                 type="text"
-//                 onChange={(e) => setQuarter(e.target.value)}
-//                 placeholder="Trimestre"
-//               />
-//               <input
-//                 name="skills"
-//                 value={skills}
-//                 className="w-full h-10 px-3 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline"
-//                 type="text"
-//                 onChange={(e) => setSkills(e.target.value)}
-//                 placeholder="Habilidades"
-//               />
-//               <input
-//                 name="interests"
-//                 value={interests}
-//                 className="w-full h-10 px-3 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline"
-//                 type="text"
-//                 onChange={(e) => setInterests(e.target.value)}
-//                 placeholder="Intereses"
-//               />
-//               <input
-//                 name="description"
-//                 value={description}
-//                 className="w-full h-10 px-3 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline"
-//                 type="text"
-//                 onChange={(e) => setDescription(e.target.value)}
-//                 placeholder="Descripción"
-//               />
-//               <button
-//                 onClick={profileUpdate}
-//                 className="w-full h-10 px-3 text-base text-white bg-gray-500 rounded-lg focus:shadow-outline"
-//               >
-//                 Guardar
-//               </button>
-//             </GridContainer>
-//           </GridSecond>
-//         </div>
-//       </ContainerWeb>
-//     </>
-//   );
-// }
-
-
-
-
-
-
-
 "use client";
 import ContainerWeb from "@/components/ContainerWeb";
 import GridContainer from "@/components/GridContainer";
@@ -243,18 +62,24 @@ export default function Page() {
           "Este es tu perfil, aquí podrás visualizar tu información personal y actualizarla si es necesario."
         }
       />
-      <ContainerWeb>
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
-          <GridMain>
-            <GridContainer>
-              {session?.user.profile ? "true" : "false"}
-              <EstudianteProfile profileData={profileData} />
-            </GridContainer>
-          </GridMain>
+    
+        <div className="grid grid-cols-1 mx-8 relative z-20 lg:grid-cols-[60%_40%] gap-2">
+         
+{/* {session?.user.profile ? "true" : "false"} */}
+             
+           
+          <div className="flex justify-center bg-white  m-4">
+            <EstudianteProfile profileData={profileData} />
+          </div>
+         
+          
+          <div className="bg-white m-4 mx-4">
+          <div className="m-4 my-4 p-4 bg-white rounded-lg shadow-lg mt-10">
+            <h2 className="text-2xl font-bold text-gray-800 mb-4 md:text-4xl lg:text-3xl">
+              Completa tu Perfil
+            </h2>
 
-          <GridSecond>
-            <GridContainer>
-              <form onSubmit={handleSubmit(profileUpdate)}>
+              <form onSubmit={handleSubmit(profileUpdate)} className="form-student-info">
                 <LabelInputContainer className="mb-4">
                   <Label htmlFor="names">Nombres</Label>
                   <Input 
@@ -381,17 +206,25 @@ export default function Page() {
                   {errors.description && <p className="text-red-500 text-sm">{errors.description.message}</p>}
                 </LabelInputContainer>
 
-                <button
-                  type="submit"
-                  className="w-full h-10 px-3 text-base text-white bg-gray-500 rounded-lg focus:shadow-outline"
+               
+
+                
+
+
+          <div className="flex justify-center">
+            <button
+                type="submit"
+                className="w-[50%] bg-black hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:shadow-outline"
                 >
-                  Guardar
-                </button>
+                Guardar
+            </button>
+          </div>
               </form>
-            </GridContainer>
-          </GridSecond>
+            </div>
+            </div>
+         
         </div>
-      </ContainerWeb>
+     
     </>
   );
 }
--- src/components/perfiles/EstudianteProfile.jsx
+++ src/components/perfiles/EstudianteProfile.jsx
@@ -4,30 +4,34 @@ import Link from 'next/link';
 
 export default function EstudianteProfile ({ profileData }) {
   return (
-    <div className="max-w-3xl mx-auto p-6 bg-white rounded-lg shadow-lg mt-10">
-      <div className="flex items-start">
-        {/* Foto del Alcalde */}
+    <div className="m-4 my-4 p-4 bg-white rounded-lg shadow-lg mt-10">
+      
+      <div className="flex flex-cols-[30%_70%] ">
+        
+        {/* Foto del Estudiante */}
         <div className="flex-shrink-0">
           <img
-            className="h-40 w-40 rounded-full border-4 border-blue-500"
+            className="h-60 w-60 rounded-full border-4 border-black-800"
             src={profileData.fotoDelEstudiante}
-            alt="Foto del Representante de Alcaldia"
+            alt="Foto del Estudiante"
           />
         </div>
-        {/* Información del Alcalde */}
+        {/* Información del Estudiante */}
         <div className="ml-4">
-          <h2 className="text-3xl font-bold text-gray-800 pt-5 text-center">{profileData.nombreEstudiante}</h2>
-          <h2 className="text-2xl font-bold text-gray-800 pt-5">{profileData.carreraEstudiante}</h2>
-          <p className="text-gray-600"><strong>Cedula de identidad:</strong> {profileData.cedulaEstudiante}</p>
-          <p className="text-gray-600"><strong>Teléfono:</strong> {profileData.telefonoEstudiante}</p>
-          <p className="text-gray-600"><strong>Correo:</strong> {profileData.correoEstudiante}</p>
-          <p className="text-gray-600"><strong>Domicilio:</strong> {profileData.domicilioEstudiante}</p>
+          <h2 className="text-3xl font-bold text-gray-800 pt-5 text-center md:text-5xl lg:text-4xl">{profileData.nombreEstudiante}</h2>
+          <h2 className="text-2xl font-bold text-gray-800 pt-5 pb-2 md:text-4xl lg:text-3xl"> <i>{profileData.carreraEstudiante}</i></h2>
+          <p className="text-gray-600 md:text-1x1"><strong>Cedula de identidad:</strong> {profileData.cedulaEstudiante}</p>
+          <p className="text-gray-600 md:text-1xl"><strong>Teléfono:</strong> {profileData.telefonoEstudiante}</p>
+          <p className="text-gray-600 md:text-1xl"><strong>Correo:</strong> {profileData.correoEstudiante}</p>
+          <p className="text-gray-600 md:text-1xl"><strong>Domicilio:</strong> {profileData.domicilioEstudiante}</p>
         </div>
       </div>
 
-    
+
+
       <div className="mt-6">
-        <h4 className="text-lg font-semibold text-gray-800">Perfil profesional</h4>
+      <h2 className="text-2xl font-bold text-gray-800 pt-5 pb-2 text-center md:text-4xl lg:text-3xl">Perfil profesional</h2>
+
         <div className="mt-2"> 
           <p className="text-gray-600"><strong>Universidad:</strong> {profileData.universidadEstudiante}</p>
           <p className="text-gray-600"><strong>trimestre:</strong> {profileData.trimestreEstudiante}</p>
@@ -35,15 +39,18 @@ export default function EstudianteProfile ({ profileData }) {
           <p className="text-gray-6000"><strong>Intereses:</strong> {profileData.interesesEstudiante}</p>
           <p className="text-gray-600"><strong>Descripción:</strong> {profileData.descripcionEstudiante}</p>
           <p className="text-gray-6000"><Link  className="underline text-blue-500 hover:text-blue-700 cursor-pointer" href={'profileData.reseumenCurricularEstudiante'}> Reseumen Curricular </Link></p>
-        </div>
       </div>
+        </div>
       <br/>
-      <button
-          type="submit"
-          className="w-full bg-black hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
-        >
-          Actualizar Datos
-        </button>
+      
+      <div className="flex justify-center">
+        <button
+            type="submit"
+            className="w-[50%] bg-black hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+            >
+            Actualizar Datos
+          </button>
+      </div>
     </div>
   );
 };
