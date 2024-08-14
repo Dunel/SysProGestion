c6d34440c40ed7ac4dac497554643fb5a31a9c96
Edily Mora <edilyrosa@gmail.com>
2024-08-14 18:09:19 -0400

    improving

--- src/app/estudiante/perfil/page.tsx
+++ src/app/estudiante/perfil/page.tsx
@@ -19,7 +19,7 @@ export default function EstudianteInfoForm() {
   const getProfile = async () => {
     try {
       setLoading(true); // Muestra el loader
-      console.log("session?.user.dataProfile:", session?.user.dataProfile);
+      //console.log("session?.user.dataProfile:", session?.user.dataProfile);
       if (!session?.user.profile || session.user.dataProfile) {
         setLoading(false);
         return;
