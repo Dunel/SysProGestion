3c29eeeeb5f626efc6ae1549cb6d25cfcfac3fbe
Edily Mora <edilyrosa@gmail.com>
2024-08-23 19:45:11 -0400

    adding icons to Notifications page

--- src/app/estudiante/notificaciones/NotificacionsCard.tsx
+++ src/app/estudiante/notificaciones/NotificacionsCard.tsx
@@ -1,5 +1,6 @@
 "use client";
-import GridContainer from "@/components/GridContainer";
+import { FaClipboardCheck, FaTrashAlt, FaRegSadCry, FaRegLaughSquint } from 'react-icons/fa';
+
 export default function NotificationsCard({noti}: {noti: any}, {index}:{index: any}) {
     const actionDesc = {
         apply: "Aplicaste a",
@@ -17,15 +18,39 @@ export default function NotificationsCard({noti}: {noti: any}, {index}:{index: a
   return (
     <>
                     <div key={index} className="w-full flex flex-col justify-center m-2 p-2 bg-[#c2dded62] shadow-md md:w-[80%]">
-                        <div className="flex flex-col">
-                            <p className="text-lg text-gray-600 mb-1">
-                                {actionDesc[noti.action as keyof typeof actionDesc]}
-                                <strong>{" "}{typeDesc[noti.application.type as keyof typeof typeDesc]}</strong> de <i>{noti.application.title}</i>
-                            </p>
-                           
-                        </div>
-                        <div className="flex flex-col">
-                        <p className="text-lg text-gray-600 mb-1">En fecha: {new Date(noti.date).toLocaleString()}</p>
+                        <div className="flex flex-row">
+
+                            <div className="flex-col w-[80%]">
+                                <div className="flex flex-col">
+                                    <p className="text-lg text-gray-600 mb-1">
+                                        {actionDesc[noti.action as keyof typeof actionDesc]}
+                                        <strong>{" "}{typeDesc[noti.application.type as keyof typeof typeDesc]}</strong> de <i>{noti.application.title}</i>
+                                    </p>
+                                
+                                </div>
+                                <div className="flex flex-col">
+                                <p className="text-lg text-gray-600 mb-1">En fecha: {new Date(noti.date).toLocaleString()}</p>
+                                </div>
+                            </div>
+
+
+                            <div className="flex justify-center w-[20%]">
+                                <button
+                                    className="m-2 text-white hover:text-red-300">
+                                        {
+                                            noti.action === 'delete' 
+                                            ? <FaTrashAlt style={{ color: 'red' }} size={40}/>
+                                            : noti.action === 'apply' 
+                                                ? <FaClipboardCheck style={{ color: 'green' }} size={40}/>
+                                                : noti.action === 'acept' 
+                                                    ? <FaRegLaughSquint style={{ color: 'green' }} size={40}/> 
+                                                    :noti.action === 'reject' 
+                                                        ? <FaRegSadCry style={{ color: 'red' }} size={40}/> 
+                                                        :''
+                                        }
+                                </button>
+                            </div>
+
                         </div>
                     </div>
                    
