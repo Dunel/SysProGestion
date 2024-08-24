e04f70a99e589c56680825eb66cdb2245e3c96c3
Daniel Amaya <daniel.amaya18@gmail.com>
2024-08-24 14:57:02 -0400

    type notifications

--- src/app/estudiante/notificaciones/page.tsx
+++ src/app/estudiante/notificaciones/page.tsx
@@ -5,7 +5,7 @@ import axios from "axios";
 import { useSession } from "next-auth/react";
 import { useState, useEffect } from "react";
 import NotificationsCard from "./NotificacionsCard";
-import Skeleton from '@/components/ui/SkeletonComponentLines';
+import Skeleton from "@/components/ui/SkeletonComponentLines";
 
 type Notificaciones = {
   action: "apply" | "reject" | "accept" | "proposal" | "delete";
@@ -18,21 +18,20 @@ type Notificaciones = {
 
 export default function Page() {
   const [squeleton, setSqueleton] = useState(true);
-  const { data: session } = useSession();
   const [notificaciones, setNotifcaciones] = useState<Notificaciones[]>();
 
   const getNotificaciones = async () => {
     try {
       setSqueleton(true);
       const res = await axios.get("/api/estudiante/notifications");
-      setNotifcaciones(res.data.notifications);      
+      setNotifcaciones(res.data.notifications);
     } catch (error) {
       if (axios.isAxiosError(error)) {
         console.log("error lanzado:", error.response?.data.error);
       } else {
         console.error("error:", error);
-      
-    }   }finally {
+      }
+    } finally {
       setSqueleton(false);
     }
   };
@@ -41,44 +40,25 @@ export default function Page() {
     getNotificaciones();
   }, []);
 
-  const actionDesc = {
-    apply: "Aplicaste a",
-    reject: "Rechazaron tú solicitud de",
-    accept: "Aceptaron tú solicitud de",
-    proposal: "Aplicaste a la propuesta de proyecto de",
-    delete: "Eliminaste tú solicitud de",
-  };
-
-  const typeDesc = {
-    servicio: "Servicio Comunitario",
-    pasantia: "Pasantía",
-    proyecto: "Proyecto",
-  };
-
   return (
     <>
-      <Header title={"NOTIFICACIONES"} subtitle={"Aqui podras visualizar todas las notificaciones relacionadas con los procesos a los que aplicaste y eres parte interesada. Mantente informado!"} />
-  
-          {squeleton && <Skeleton/>}
-          <div className="flex flex-col items-center bg-white rounded-lg shadow-md m-4 mb-8 p-2 w-[90%] mx-auto my-5"> 
-             
-              
-              {notificaciones && notificaciones.length > 0 
-              ? (
-                notificaciones.map((noti) => (
-                  <NotificationsCard noti={noti} />
-                ))
-                ) 
-              : (
-                <GridContainer>
-                  <p>No hay notificaciones disponibles</p>
-                </GridContainer>
-              )}
-           
+      <Header
+        title={"NOTIFICACIONES"}
+        subtitle={
+          "Aqui podras visualizar todas las notificaciones relacionadas con los procesos a los que aplicaste y eres parte interesada. Mantente informado!"
+        }
+      />
 
-        
-        </div>
-
+      {squeleton && <Skeleton />}
+      <div className="flex flex-col items-center bg-white rounded-lg shadow-md m-4 mb-8 p-2 w-[90%] mx-auto my-5">
+        {notificaciones && notificaciones.length > 0 ? (
+          notificaciones.map((noti) => <NotificationsCard noti={noti} />)
+        ) : (
+          <GridContainer>
+            <p>No hay notificaciones disponibles</p>
+          </GridContainer>
+        )}
+      </div>
     </>
   );
 }
