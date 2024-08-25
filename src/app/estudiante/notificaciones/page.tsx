b1f2206802559d2cbbcc5e1e482c88f1b5ecb29c
Edily Mora <edilyrosa@gmail.com>
2024-08-25 13:40:34 -0400

    doing ofertas page

--- src/app/estudiante/notificaciones/page.tsx
+++ src/app/estudiante/notificaciones/page.tsx
@@ -60,9 +60,8 @@ export default function Page() {
       <Header title={"NOTIFICACIONES"} subtitle={"Aqui podras visualizar todas las notificaciones relacionadas con los procesos a los que aplicaste y eres parte interesada. Mantente informado!"} />
   
           {squeleton && <Skeleton/>}
+          
           <div className="flex flex-col items-center bg-white rounded-lg shadow-md m-4 mb-8 p-2 w-[90%] mx-auto my-5"> 
-             
-              
               {notificaciones && notificaciones.length > 0 
               ? (
                 notificaciones.map((noti) => (
@@ -70,13 +69,12 @@ export default function Page() {
                 ))
                 ) 
               : (
-                <GridContainer>
-                  <p>No hay notificaciones disponibles</p>
-                </GridContainer>
+                <div className="flex flex-col justify-center items-center mt-12 mx-auto bg-white w-[60%] min-h-[30vh]">
+                    <p className="m-2 p-2 text-center text-red-500">No tienes Notificaciones.</p>
+                    <p className="m-2 p-2 text-center">Entra a tu menú de navegación y haz clic en la opción <i>“Oferta de Vacantes”, </i> y sé parte de procesos!</p>
+                  </div>
               )}
            
-
-        
         </div>
 
     </>
---
+++ src/app/estudiante/ofertas/page.tsx
@@ -0,0 +1,112 @@
+"use client";
+import Header from "@/components/Header";
+import axios from "axios";
+import { useSession } from "next-auth/react";
+import { useState, useEffect } from "react";
+import Skeleton from '@/components/ui/SkeletonComponentLines';
+import OffertCards from '@/app/estudiante/ofertas/OffertCards'
+
+
+type Application = {
+  id: number;
+  title: string;
+  type: "pasantia" | "servicio" | "proyecto";
+  description: string;
+  imagen: string;
+  skills: string[];
+  date: Date;
+  location: string;
+  status: string;
+  apply: [
+    {
+      id: number;
+    }
+  ];
+};
+
+export default function Page() {
+  const { data: session } = useSession();
+  const [applications, setApplications] = useState<Application[]>();
+  const [squeleton, setSqueleton] = useState(true);
+
+
+  const handleApply = async (id: number) => {
+    try {
+      const res = await axios.post("/api/estudiante/apply", { id });
+      console.log(res.data);
+      getApplications();
+      alert("Solicitud enviada");
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        console.log("error lanzado:", error.response?.data.error);
+      } else {
+        console.error("error:", error);
+      }
+    }
+  };
+
+  const handleDeleteApply = async (idAplication: number, applyId: number) => {
+    try {
+      const res = await axios.delete("/api/estudiante/apply", {
+        data: { idAplication, applyId },
+      });
+      console.log(res.data);
+      getApplications();
+      alert("Solicitud eliminada");
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        console.log("error lanzado:", error.response?.data.error);
+      } else {
+        console.error("error:", error);
+      }
+    }
+  };
+
+  const getApplications = async () => {
+    try {
+      const res = await axios.get("/api/estudiante/apply");
+      setApplications(res.data.applications);
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        console.log("error lanzado:", error.response?.data.error);
+      } else {
+        console.error("error:", error);
+      }
+    }
+  };
+  useEffect(() => {
+    getApplications();
+  }, []);
+
+  const typeDesc = {
+    pasantia: "Pasantía",
+    servicio: "Servicio Comunitario",
+    proyecto: "Proyecto",
+  };
+
+  console.log('Application', applications);
+  
+  return (
+    <>
+      <Header title={"OFERTAS DE VACANTES"} subtitle={"Aquí podras ver todas las ofertas de Vacante de Pasantias y Servicio Comunitarios que las Dependencias han publicado"} />
+      {squeleton && <Skeleton/>}
+
+      <div className="flex flex-col items-center bg-white rounded-lg shadow-md m-4 mb-8 p-2 w-[90%] mx-auto my-5"> 
+            {applications && applications.length > 0 
+              ? (
+                applications.map((noti) => (
+                  // <OffertCards noti={noti} />
+                  <div />
+                ))
+                ) 
+              : (
+                <div className="flex flex-col justify-center items-center mt-12 mx-auto bg-white w-[60%] min-h-[30vh]">
+                    <p className="m-2 p-2 text-center text-red-500">No tienes Notificaciones.</p>
+                    <p className="m-2 p-2 text-center">Entra a tu menú de navegación y haz clic en la opción <i>“Oferta de Vacantes”, </i> y sé parte de procesos!</p>
+                  </div>
+              )}
+           
+        </div>
+    </>
+  );
+}
