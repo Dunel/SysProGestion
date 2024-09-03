8306c58178b7378ebb6b27fa9983a28b61bfee92
Edily Mora <edilyrosa@gmail.com>
2024-09-03 17:04:44 -0400

    agregando vista Offertcards

--- src/app/estudiante/ofertas/OffertCards.tsx
+++ src/app/estudiante/ofertas/OffertCards.tsx
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 
-interface Skill {
-  id: number;
-  name: string;
-}
-
 interface Internship {
   handleDeleteApply: Function;
-  dependencia: string;
+  handleAcceptApply: Function;
+  dependencia: {
+    name: string;
+    User: {
+      image: string;
+    };
+  };
   id: number;
   title: string;
   description: string;
@@ -25,135 +26,162 @@ interface Internship {
   ];
 }
 
-const skillFormated : { [key: string]: string } = {
+const skillFormated: { [key: string]: string } = {
   resoluciondeproblemas: "Resolucion de problemas",
-    trabajoenequipo: "Trabajo en equipo",
-    adaptabilidad: "Adaptabilidad",
-    comunicacionefectiva: "Comunicación efectiva",
-    liderazgo: "Liderazgo",
-    pensamientocritico: "Pensamiento crítico",
-    orientacionaresultados: "Orientación a resultados",
-    creatividad: "Creatividad",
-    gestiondeltiempo: "Gestión del tiempo",
-    aprendizajecontinuo: "Aprendizaje continuo",
-    dondegente: "Don de gente",
-    ensenanza: "Enseñanza",
-    sociable: "Sociable",
-    salud: "Salud",
-    deportes: "Deportes",
-    logistica: "Logística",
-    expresionesartisticas: "Expresiones artísticas",
-    diseno: "Diseño",
-    musica: "Música",
-    ingles: "Inglés",
-    otrosidiomasnaturales: "Otros idiomas naturales",
-    lenguajesdeprogramacion: "Lenguajes de programación",
-}
+  trabajoenequipo: "Trabajo en equipo",
+  adaptabilidad: "Adaptabilidad",
+  comunicacionefectiva: "Comunicación efectiva",
+  liderazgo: "Liderazgo",
+  pensamientocritico: "Pensamiento crítico",
+  orientacionaresultados: "Orientación a resultados",
+  creatividad: "Creatividad",
+  gestiondeltiempo: "Gestión del tiempo",
+  aprendizajecontinuo: "Aprendizaje continuo",
+  dondegente: "Don de gente",
+  ensenanza: "Enseñanza",
+  sociable: "Sociable",
+  salud: "Salud",
+  deportes: "Deportes",
+  logistica: "Logística",
+  expresionesartisticas: "Expresiones artísticas",
+  diseno: "Diseño",
+  musica: "Música",
+  ingles: "Inglés",
+  otrosidiomasnaturales: "Otros idiomas naturales",
+  lenguajesdeprogramacion: "Lenguajes de programación",
+};
 
-const InternshipCard: React.FC<{ internship: Internship }> = ({
-  internship,
-}) => (
-  <div className="flex flex-col justify-center bg-white rounded-lg shadow-md m-4 mb-8 p-2 w-[90%] mx-auto my-5">
-    <div className="flex">
-      {/* //!ESTE CODE DEBERIA VENIR DE UN CAPO DE LA TABLA "ofertas" CUYA NOMENCLATURA SE CREA DE SEGUN EL TIPO DE OFERTA + ANO + ID   */}
-      <span className="flex  ml-auto p-1 text-red-500">
-        Codigo de Oferta de Vacante: {"P-2024-000" + internship.id}
-      </span>
-    </div>
-    <div className="flex flex-col items-center md:flex-row md:space-x-4">
-      {/* //! IMG */}
-      <div className="flex m-1 p-1 mx-auto h-[40%] md:w-[30%] lg:w-[20%]">
-        <img
-          src={internship.imagen}
-          alt={`${internship.dependencia} logo`}
-          className="mx-auto w-60 h-60 object-cover rounded-full border-4 border-black-800 md:w-40 md:h-40"
-        />
-      </div>
-
-      {/* //! INFO */}
-      <div className="m-1 p-1 word-wrap overflow-wrap h-[60%] md:w-[80%]">
-        <h3 className="text-xl font-semibold text-gray-800 mb-2">
-          {internship.title}
-        </h3>
-        <p className="text-lg text-gray-600 mb-1">
-          {" "}
-          <i>{internship.dependencia}</i>
-        </p>
-        <p className="text-sm text-gray-500">📍{internship.location}</p>
-
-        <div className="flex my-2 gap-2">
-          <div className="w-[33%]">
-            <span className="text-lg font-medium text-gray-700 mb-2">
-              Estado de la Solicitud:
-            </span>
-            <p>{internship.status}</p>
-          </div>
-          <div className="w-[33%]">
-            <span className="text-lg font-medium text-gray-700 mb-2">
-              Estado de tu Aplicacion:
-            </span>
-            <p>{internship.apply[0].status}</p>
-          </div>
-          <div className="w-[33%]">
-            <span className="text-lg font-medium text-gray-700 mb-2">
-              Tipo de Oferta:
-            </span>
-            {/* //! ASI QUE NO DEBERIA SER {internship.type} DADO QUE EL TYPO DEL PROCEDIMIENTO LO DEFINE LA OFERTA NO EL QUE UN ESTUDIANTE APLIQUE A ELLA */}
-            {/* //! PROYECTO DE TESIS DEBERIA ESTAR EN OFERTAS? NO LO CREO */}
-            <p>
-              {internship.type === "pasantia"
-                ? "Pasantias"
-                : internship.type === "servicio"
-                ? "Servicio Cominitario"
-                : internship.type === "proyecto"
-                ? "Proyecto de Tesis"
-                : ""}
-            </p>
-          </div>
-        </div>
-
-        {/* //! CONTAINER FLEX: OF SKILLS AND DESCRIPTION */}
-        <div className="flex flex-col justify-center gap-2 my-2 lg:flex-row">
-          <div className="m-1 w-[100%] lg:w-[40%]">
-            <h4 className="text-lg font-medium text-gray-700 mb-2">
-              Habilidades requeridas 📝
-            </h4>
-            <ul className="list-disc list-inside">
-              {internship.skills.map((skill, index) => (
-                <li key={index} className="text-gray-600">
-                  {skillFormated[skill]}
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div className="m-1 w-[100%] lg:w-[60%]">
-            <h4 className="text-lg font-medium text-gray-700 mb-2">
-              Descripcion de la Vacante 📋
-            </h4>
-            <p>{internship.description}</p>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div className="flex justify-center">
-      <button
-        onClick={() =>
-          internship.handleDeleteApply(internship.id, internship.apply[0].id)
-        }
-        className="w-[100%] p-1 m-1 bg-red-500 hover:bg-red-600 text-white font-bold rounded transition duration-300 md:w-[50%]"
-      >
-        Retirar aplicación
-      </button>
-    </div>
-  </div>
-);
+const statusFormated: { [key: string]: string } = {
+  pendiente: "Pendiente",
+  aceptado: "Aceptado",
+  rechazado: "Rechazado",
+  declinado: "Declinado",
+  aprobado: "Aprobado",
+};
 
 export default function InternshipCards({
   internships,
 }: {
   internships: Internship[];
 }) {
+  const InternshipCard: React.FC<{ internship: Internship }> = ({
+    internship,
+  }) => (
+    <div className="flex flex-col justify-center bg-white rounded-lg shadow-md m-4 mb-8 p-2 w-[90%] mx-auto my-5">
+      <div className="flex">
+        {/* //!ESTE CODE DEBERIA VENIR DE UN CAPO DE LA TABLA "ofertas" CUYA NOMENCLATURA SE CREA DE SEGUN EL TIPO DE OFERTA + ANO + ID   */}
+        <span className="flex  ml-auto p-1 text-red-500">
+          Codigo de Oferta de Vacante: {"P-2024-000" + internship.id}
+        </span>
+      </div>
+      <div className="flex flex-col items-center md:flex-row md:space-x-4">
+        {/* //! IMG */}
+        <div className="flex m-1 p-1 mx-auto h-[40%] md:w-[30%] lg:w-[20%]">
+          <img
+            src={internship.dependencia.User.image}
+            alt={`${internship.dependencia} logo`}
+            className="mx-auto w-60 h-60 object-cover rounded-full border-4 border-black-800 md:w-40 md:h-40"
+          />
+        </div>
+
+        {/* //! INFO */}
+        <div className="m-1 p-1 word-wrap overflow-wrap h-[60%] md:w-[80%]">
+          <h3 className="text-xl font-semibold text-gray-800 mb-2">
+            {internship.title}
+          </h3>
+          <p className="text-lg text-gray-600 mb-1">
+            {" "}
+            <i>{internship.dependencia.name}</i>
+          </p>
+          <p className="text-sm text-gray-500">📍{internship.location}</p>
+
+          <div className="flex my-2 gap-2">
+            <div className="w-[33%]">
+              <span className="text-lg font-medium text-gray-700 mb-2">
+                Estado de la Solicitud:
+              </span>
+              <p>{internship.status}</p>
+            </div>
+            <div className="w-[33%]">
+              <span className="text-lg font-medium text-gray-700 mb-2">
+                Estudiantes que han aplicado:
+              </span>
+               <p>{internship.status}</p>
+              <p>AQUI EL ESTADO</p>
+            </div>
+            <div className="w-[33%]">
+              <span className="text-lg font-medium text-gray-700 mb-2">
+                Tipo de Oferta:
+              </span>
+              {/* //! ASI QUE NO DEBERIA SER {internship.type} DADO QUE EL TYPO DEL PROCEDIMIENTO LO DEFINE LA OFERTA NO EL QUE UN ESTUDIANTE APLIQUE A ELLA */}
+              {/* //! PROYECTO DE TESIS DEBERIA ESTAR EN OFERTAS? NO LO CREO */}
+              <p>
+                {internship.type === "pasantia"
+                  ? "Pasantias"
+                  : internship.type === "servicio"
+                  ? "Servicio Cominitario"
+                  : internship.type === "proyecto"
+                  ? "Proyecto de Tesis"
+                  : ""}
+              </p>
+            </div>
+          </div>
+
+          {/* //! CONTAINER FLEX: OF SKILLS AND DESCRIPTION */}
+          <div className="flex flex-col justify-center gap-2 my-2 lg:flex-row">
+            <div className="m-1 w-[100%] lg:w-[40%]">
+              <h4 className="text-lg font-medium text-gray-700 mb-2">
+                Habilidades requeridas 📝
+              </h4>
+              <ul className="list-disc list-inside">
+                {internship.skills.map((skill, index) => (
+                  <li key={index} className="text-gray-600">
+                    {skillFormated[skill]}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="m-1 w-[100%] lg:w-[60%]">
+              <h4 className="text-lg font-medium text-gray-700 mb-2">
+                Descripcion de la Vacante 📋
+              </h4>
+              <p>{internship.description}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-center">
+        {/* {internship.apply[0].status === "aprobado" && (
+          <button
+            onClick={() =>
+              internship.handleAcceptApply(
+                internship.id,
+                internship.apply[0].id
+              )
+            }
+            className="w-[100%] p-1 m-1 bg-green-500 hover:bg-green-600 text-white font-bold rounded transition duration-300 md:w-[50%]"
+          >
+            Aceptar Oferta
+          </button>
+        )} */}
+
+        {/* {internship.apply[0].status === "aprobado" ||
+        internship.apply[0].status === "pendiente" ? (
+          <button
+            onClick={() =>
+              internship.handleDeleteApply(
+                internship.id,
+                internship.apply[0].id
+              )
+            }
+            className="w-[100%] p-1 m-1 bg-red-500 hover:bg-red-600 text-white font-bold rounded transition duration-300 md:w-[50%]"
+          >
+            Retirar aplicación
+          </button>
+        ) : null} */}
+      </div>
+    </div>
+  );
   return (
     <>
       <div className="relative z-20 mx-auto py-2 rounded shadow w-[90%]">
--- src/app/estudiante/ofertas/page.tsx
+++ src/app/estudiante/ofertas/page.tsx
@@ -1,108 +1,19 @@
-// "use client";
-// import Header from "@/components/Header";
-// import axios from "axios";
-// import { useSession } from "next-auth/react";
-// import { useState, useEffect } from "react";
-// import Skeleton from '@/components/ui/SkeletonComponentLines';
-// import OffertCards from '@/app/estudiante/ofertas/OffertCards'
-
-
-// type Application = {
-//   id: number;
-//   title: string;
-//   type: "pasantia" | "servicio" | "proyecto";
-//   description: string;
-//   imagen: string;
-//   skills: string[];
-//   date: Date;
-//   location: string;
-//   status: string;
-//   apply: [
-//     {
-//       id: number;
-//     }
-//   ];
-// };
-
-// export default function Page() {
-//   const { data: session } = useSession();
-//   const [applications, setApplications] = useState<Application[]>();
-//   const [squeleton, setSqueleton] = useState(true);
-
-
-//   const handleApply = async (id: number) => {
-//     try {
-//       const res = await axios.post("/api/estudiante/apply", { id });
-//       console.log(res.data);
-//       getApplications();
-//       alert("Solicitud enviada");
-//     } catch (error) {
-//       if (axios.isAxiosError(error)) {
-//         console.log("error lanzado:", error.response?.data.error);
-//       } else {
-//         console.error("error:", error);
-//       }
-//     }
-//   };
-
-//   const handleDeleteApply = async (idAplication: number, applyId: number) => {
-//     try {
-//       const res = await axios.delete("/api/estudiante/apply", {
-//         data: { idAplication, applyId },
-//       });
-//       console.log(res.data);
-//       getApplications();
-//       alert("Solicitud eliminada");
-//     } catch (error) {
-//       if (axios.isAxiosError(error)) {
-//         console.log("error lanzado:", error.response?.data.error);
-//       } else {
-//         console.error("error:", error);
-//       }
-//     }
-//   };
-
-//   const getApplications = async () => {
-//     try {
-//       const res = await axios.get("/api/estudiante/apply");
-//       setApplications(res.data.applications);
-//     } catch (error) {
-//       if (axios.isAxiosError(error)) {
-//         console.log("error lanzado:", error.response?.data.error);
-//       } else {
-//         console.error("error:", error);
-//       }
-//     }
-//   };
-//   useEffect(() => {
-//     getApplications();
-//   }, []);
-
-//   const typeDesc = {
-//     pasantia: "Pasantía",
-//     servicio: "Servicio Comunitario",
-//     proyecto: "Proyecto",
-//   };
-
-//   console.log('Application', applications);
- 
 "use client";
 import Header from "@/components/Header";
 import axios from "axios";
-import { useSession } from "next-auth/react";
 import { useState, useEffect } from "react";
-import Skeleton from '@/components/ui/SkeletonComponentLines';
-import InternshipCards from "./OffertCards";
-
+import OffertCards from "@/app/estudiante/ofertas/OffertCards";
+import Skeleton from "@/components/ui/SkeletonComponent";
 
 type Application = {
   id: number;
   title: string;
-  type: "pasantia" | "servicio" | "proyecto";
   description: string;
-  skills: string[];
-  date: Date;
   location: string;
+  type: "pasantia" | "servicio" | "proyecto";
+  imagen: string;
+  date: Date;
+  skills: string[];
   status: string;
   dependencia: {
     name: string;
@@ -113,14 +24,16 @@ type Application = {
   apply: [
     {
       id: number;
+      status: string;
     }
   ];
 };
 
 export default function Page() {
-  const { data: session } = useSession();
   const [applications, setApplications] = useState<Application[]>();
   const [squeleton, setSqueleton] = useState(true);
+
+
 
   const handleApply = async (id: number) => {
     try {
@@ -134,23 +47,8 @@ export default function Page() {
       } else {
         console.error("error:", error);
       }
-    }
-  };
-
-  const handleDeleteApply = async (idAplication: number, applyId: number) => {
-    try {
-      const res = await axios.delete("/api/estudiante/apply", {
-        data: { idAplication, applyId },
-      });
-      console.log(res.data);
-      getApplications();
-      alert("Solicitud eliminada");
-    } catch (error) {
-      if (axios.isAxiosError(error)) {
-        console.log("error lanzado:", error.response?.data.error);
-      } else {
-        console.error("error:", error);
-      }
+    }finally{
+      setSqueleton(false)
     }
   };
 
@@ -165,10 +63,14 @@ export default function Page() {
       } else {
         console.error("error:", error);
       }
+    }finally{
+      setSqueleton(false)
     }
   };
+  
   useEffect(() => {
     getApplications();
+    
   }, []);
 
   const typeDesc = {
@@ -176,37 +78,40 @@ export default function Page() {
     servicio: "Servicio Comunitario",
     proyecto: "Proyecto",
   };
+  
   return (
     <>
-      <Header title={"OFERTAS DE VACANTES"} subtitle={"Aquí podras ver todas las ofertas de Vacante de Pasantias y Servicio Comunitarios que las Dependencias han publicado"} />
-      {squeleton && <Skeleton/>}
+      <Header
+        title={"TODAS LAS OFERTAS DE VACANTES"}
+        subtitle={
+          "... Vacantes de Pasantías, Servicio Comunitario y Proyectos de tesis a las que has aplicado exitosamente."
+        }
+      />
 
-      <div className="flex flex-col items-center bg-white rounded-lg shadow-md m-4 mb-8 p-2 w-[90%] mx-auto my-5"> 
-            {applications && applications.length > 0 
-              ? (
-                applications.map((noti) => (
-
-                      //           <InternshipCards
-                      //             internships={applications.map((internship) => ({
-                      //             ...internship,
-                      //             handleDeleteApply:confirmDelete, // Pasamos la función confirmDelete
-                      //             handleApply:handleApply,
-                      //             flagOffer:true //Funcion de vista Oferta
-                      //   }))}
-                      // />
-                            
-                    <>hola</>
-
-                ))
-                ) 
-              : (
-                <div className="flex flex-col justify-center items-center mt-12 mx-auto bg-white w-[60%] min-h-[30vh]">
-                    <p className="m-2 p-2 text-center text-red-500">No tienes Notificaciones.</p>
-                    <p className="m-2 p-2 text-center">Entra a tu menú de navegación y haz clic en la opción <i>“Oferta de Vacantes”, </i> y sé parte de procesos!</p>
-                  </div>
-              )}
-           
+      {squeleton ? (
+        <Skeleton />
+      ) : applications && applications.length === 0 ? (
+        <div className="flex flex-col justify-center items-center mt-12 mx-auto bg-white w-[60%] min-h-[30vh]">
+          <p className="m-2 p-2 text-center text-red-500">
+            No tienes aplicaciones a Oferta de Vacante.
+          </p>
+          <p className="m-2 p-2 text-center">
+            Entra a tu menú de navegación y haz clic en la opción{" "}
+            <i>“Oferta de Vacantes”, </i> y ¡Aplica ya!
+          </p>
         </div>
+      ) : applications && applications.length > 0 ? (
+        <OffertCards
+          internships={applications.map((internship) => ({
+            ...internship,
+            handleDeleteApply: ()=>{}, // Pasamos la función confirmDelete
+            handleAcceptApply: handleApply,
+           
+          }))}
+        />
+      ) : null}
+
+  
     </>
   );
 }
