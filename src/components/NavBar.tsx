3288906146e0ff891e7edbee9b9833f5b2b92708
Edily Mora <edilyrosa@gmail.com>
2024-10-09 17:06:05 -0400

    imporving navBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,203 @@
+// "use client";
+// import { Disclosure } from "@headlessui/react";
+// import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+// import { signOut, useSession } from "next-auth/react";
+// import Link from "next/link";
+// import { usePathname } from "next/navigation";
+// import { useEffect, useState } from "react";
+
+// function classNames(...classes: string[]) {
+//   return classes.filter(Boolean).join(" ");
+// }
+
+// export default function Navbar() {
+//   const { data: session, status } = useSession();
+//   const [navigation, setNavigation] = useState([
+//     { name: "PRINCIPAL", href: "/", current: false },
+//     { name: "INICIAR SESIÓN", href: "/login", current: false },
+//     { name: "REGÍSTRATE AQUÍ", href: "/register", current: false },
+//   ]);
+
+//   const getLinks = () => {
+//     if (status !== "authenticated") return;
+
+//     if (session?.user.role === "estudiante") {
+//       setNavigation([
+//         { name: "PRINCIPAL", href: "/estudiante", current: false },
+//         { name: "MI PERFIL", href: "/estudiante/perfil", current: false },
+//         {
+//           name: "OFERTAS DE VACANTES",
+//           href: "/estudiante/ofertas",
+//           current: false,
+//         },
+//         {
+//           name: "MIS APLICACIONES A OFERTAS",
+//           href: "/estudiante/misaplicaciones",
+//           current: false,
+//         },
+//         {
+//           name: "NOTIFICACIONES",
+//           href: "/estudiante/notificaciones",
+//           current: false,
+//         },
+//       ]);
+//     } else if (session?.user.role === "alcaldia") {
+//       setNavigation([
+//         { name: "PRINCIPAL", href: "/alcaldia", current: false },
+//         { name: "MI PERFIL", href: "/alcaldia/perfil", current: false },
+//         { name: "OFERTAS", href: "/alcaldia/ofertas", current: false },
+//         {
+//           name: "NOTIFICACIONES",
+//           href: "/alcaldia/notificaciones",
+//           current: false,
+//         },
+//         { name: "ESTUDIANTES", href: "/alcaldia/estudiantes", current: false },
+//         {
+//           name: "INSTITUCIONES",
+//           href: "/alcaldia/instituciones",
+//           current: false,
+//         },
+//         { name: "PREREGISTROS", href: "/alcaldia/preregister", current: false },
+//         {name: "CARRERAS", href: "/alcaldia/career", current: false},
+//       ]);
+//     } else if (session?.user.role === "dependencia") {
+//       setNavigation([
+//         { name: "PRINCIPAL", href: "/dependencia", current: false },
+//         { name: "MI PERFIL", href: "/dependencia/perfil", current: false },
+//         {
+//           name: "MIS OFERTAS",
+//           href: "/dependencia/misofertas",
+//           current: false,
+//         },
+//         {
+//           name: "CREAR OFERTA",
+//           href: "/dependencia/misofertas/crearoferta",
+//           current: false,
+//         },
+//         {
+//           name: "NOTIFICACIONES",
+//           href: "/dependencia/notificaciones",
+//           current: false,
+//         },
+//       ]);
+//     }
+//   };
+
+//   useEffect(() => {
+//     getLinks();
+//   }, [status]);
+
+//   const pathname = usePathname();
+
+//   return (
+//     <Disclosure as="nav" className="bg-gray-950 w-full h-auto min-h-[10%]">
+//       {({ open }) => (
+//         <>
+//           <div className="mx-auto px-2 sm:px-6 lg:px-8">
+//             <div className="relative flex h-16 items-center justify-between">
+//               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+//                 {/* Mobile menu button */}
+//                 <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+//                   <span className="absolute -inset-0.5" />
+//                   <span className="sr-only">Abrir menu Principal</span>
+//                   {open ? (
+//                     <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+//                   ) : (
+//                     <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+//                   )}
+//                 </Disclosure.Button>
+//               </div>
+
+//               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+//                 <div className="flex flex-shrink-0 items-center">
+//                   <Link href="/">
+//                     <img
+//                       className="h-10 w-auto"
+//                       src="/logo.png"
+//                       alt="Your Company"
+//                     />
+//                   </Link>
+//                 </div>
+
+//                 <div className="hidden sm:ml-6 sm:block">
+//                   <div className="flex space-x-4">
+//                     {navigation.map((item) => (
+//                       <Link
+//                         key={item.name}
+//                         href={item.href}
+//                         className={classNames(
+//                           pathname === item.href
+//                             ? "bg-gray-900 text-white"
+//                             : "text-gray-300 hover:bg-gray-900 hover:text-white",
+//                           "rounded-md px-3 py-2 text-sm font-medium"
+//                         )}
+//                         aria-current={
+//                           pathname === item.href ? "page" : undefined
+//                         }
+//                       >
+//                         {item.name}
+//                       </Link>
+//                     ))}
+//                     {session && (
+//                       <button
+//                         className={
+//                           "text-gray-300 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+//                         }
+//                         onClick={() => signOut()}
+//                       >
+//                         CERRAR SESIÓN
+//                       </button>
+//                     )}
+//                   </div>
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+
+//           <Disclosure.Panel className="sm:hidden">
+//             <div className="space-y-1 px-2 pb-3 pt-2">
+//               {navigation.map((item) => (
+//                 <Disclosure.Button
+//                   key={item.name}
+//                   as="a"
+//                   href={item.href}
+//                   className={classNames(
+//                     pathname === item.href
+//                       ? "bg-gray-900 text-white"
+//                       : "text-gray-300 hover:bg-gray-700 hover:text-white",
+//                     "block rounded-md px-3 py-2 text-base font-medium"
+//                   )}
+//                   aria-current={pathname === item.href ? "page" : undefined}
+//                 >
+//                   {item.name}
+//                 </Disclosure.Button>
+//               ))}
+//               {session && (
+//                 <button
+//                   className={
+//                     "text-gray-300 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+//                   }
+//                   onClick={() => signOut()}
+//                 >
+//                   CERRAR SESIÓN
+//                 </button>
+//               )}
+//             </div>
+//           </Disclosure.Panel>
+//         </>
+//       )}
+//     </Disclosure>
+//   );
+// }
+
+
+
+
+
+
 "use client";
 import { Disclosure } from "@headlessui/react";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { Bars3Icon, XMarkIcon, ChevronDownIcon } from "@heroicons/react/24/outline";
 import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -15,6 +212,7 @@ export default function Navbar() {
   const [navigation, setNavigation] = useState([
     { name: "PRINCIPAL", href: "/", current: false },
     { name: "INICIAR SESIÓN", href: "/login", current: false },
+    { name: "REGÍSTRATE AQUÍ", href: "/register", current: false },
   ]);
 
   const getLinks = () => {
@@ -57,7 +255,7 @@ export default function Navbar() {
           current: false,
         },
         { name: "PREREGISTROS", href: "/alcaldia/preregister", current: false },
-        {name: "CARRERAS", href: "/alcaldia/career", current: false},
+        { name: "CARRERAS", href: "/alcaldia/career", current: false },
       ]);
     } else if (session?.user.role === "dependencia") {
       setNavigation([
@@ -92,7 +290,7 @@ export default function Navbar() {
     <Disclosure as="nav" className="bg-gray-950 w-full h-auto min-h-[10%]">
       {({ open }) => (
         <>
-          <div className="mx-auto bg-red px-2 sm:px-6 lg:px-8">
+          <div className="mx-auto px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button */}
@@ -118,36 +316,53 @@ export default function Navbar() {
                   </Link>
                 </div>
 
-                <div className="hidden sm:ml-6 sm:block">
-                  <div className="flex space-x-4">
-                    {navigation.map((item) => (
-                      <Link
-                        key={item.name}
-                        href={item.href}
-                        className={classNames(
-                          pathname === item.href
-                            ? "bg-gray-900 text-white"
-                            : "text-gray-300 hover:bg-gray-900 hover:text-white",
-                          "rounded-md px-3 py-2 text-sm font-medium"
-                        )}
-                        aria-current={
-                          pathname === item.href ? "page" : undefined
-                        }
-                      >
-                        {item.name}
-                      </Link>
-                    ))}
-                    {session && (
-                      <button
-                        className={
-                          "text-gray-300 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
-                        }
-                        onClick={() => signOut()}
-                      >
-                        CERRAR SESIÓN
-                      </button>
-                    )}
-                  </div>
+                {/* <div className="hidden xl:flex space-x-4 sm:ml-6 sm:block w-full justify-evenly"> */}
+                <div className="space-x-4 w-full sm:ml-6 md:flex justify-evenly">
+                  {/* First 5 menu items */}
+                  {navigation.slice(0, 5).map((item) => (
+                    <Link
+                      key={item.name}
+                      href={item.href}
+                      className={classNames(
+                        pathname === item.href
+                          ? "bg-gray-900 text-white underline"
+                          : "text-gray-300 hover:bg-gray-900 hover:text-white",
+                        "text-red rounded-md px-3 py-2 font-medium text-xs md:text-sm lg:text-base"
+                      )}
+                      aria-current={pathname === item.href ? "page" : undefined}
+                    >
+                      {item.name}
+                    </Link>
+                  ))}
+
+                  {/* //! Icon for more options */}
+                  {navigation.length > 5 && (
+                    <div className="relative group">
+                      <div className="inline-block flex items-center text-gray-300 hover:text-white cursor-pointer">
+                        <ChevronDownIcon className="h-5 w-5" />
+                        <span className="ml-1 mt-2 inline-block bg-gray-900 text-white">Más</span>
+                      </div>
+                      {/* CAJA DESPLEGABLE */}
+                      <div className="w-[500%] hidden group-hover:block absolute z-50 mt-2 py-2 ml-[-100%] bg-black rounded-md shadow-lg">
+                        {navigation.slice(5).map((item) => (
+                          <Link
+                            key={item.name}
+                            href={item.href}
+                            // className="block px-4 py-2 text-sm text-gray-300 hover:bg-gray-900 hover:text-white rounded-md"
+                            className={classNames(
+                              pathname === item.href
+                                ? "bg-gray-900 text-white underline"
+                                : "text-gray-300 hover:bg-gray-900 hover:text-white",
+                              "block rounded-md px-3 py-2 text-sm font-medium"
+                            )}
+                          >
+                            {item.name}
+                          </Link>
+                        ))}
+                      </div>
+
+                    </div>
+                  )}
                 </div>
               </div>
             </div>
@@ -173,9 +388,7 @@ export default function Navbar() {
               ))}
               {session && (
                 <button
-                  className={
-                    "text-gray-300 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
-                  }
+                  className="text-gray-300 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
                   onClick={() => signOut()}
                 >
                   CERRAR SESIÓN
--- src/components/ui/NavBarEmergente.tsx
+++ src/components/ui/NavBarEmergente.tsx
@@ -1,14 +1,135 @@
-"use client"
+// "use client"
+// import React, { useState, useEffect } from 'react';
+// import { FaTimes, FaHome, FaAddressCard, FaClipboardList, FaClipboardCheck, FaMailBulk, FaExpeditedssl } from 'react-icons/fa';
+// import Link from 'next/link';
+// import { useNavBar } from '@/context/NavBarContext';
+// import { signOut, useSession } from "next-auth/react";
+
+
+// const NavBarEmergente: React.FC = () => {
+//   const [isHovered, setIsHovered] = useState(false);
+//   const { isOpen, toggleNavBar } = useNavBar();
+
+//   useEffect(() => {
+//     const handleEscape = (event: KeyboardEvent) => {
+//       if (event.key === 'Escape') {
+//         toggleNavBar();
+//       }
+//     };
+
+//     document.addEventListener('keydown', handleEscape);
+//     return () => {
+//       document.removeEventListener('keydown', handleEscape);
+//     };
+//   }, [toggleNavBar]);
+
+//   if (!isOpen) return null;
+
+//   return (
+//     <div
+//       className={`fixed top-[8vh] left-0 h-full opacidad text-white transition-all duration-300 ease-in-out z-50 ${
+//         isHovered ? 'w-64' : 'w-16'
+//       } overflow-hidden`}
+//       onMouseEnter={() => setIsHovered(true)}
+//       onMouseLeave={() => setIsHovered(false)}
+//     >
+//       <button
+//         onClick={toggleNavBar}
+//         className="absolute top-4 left-4 text-white hover:text-red-300"
+//       >
+//         <FaTimes size={24}/>
+//       </button>
+//       <nav className="pt-16">
+//         <ul className="space-y-4">
+//           <NavItem icon={<FaHome size={20} />} text="Principal" href="/estudiante" isHovered={isHovered} />
+//           <NavItem icon={<FaAddressCard size={20} />} text="Perfil" href="/estudiante/perfil" isHovered={isHovered} />
+//           <NavItem icon={<FaClipboardList size={20} />} text="Ofertas de Vacante" href="/estudiante/ofertas" isHovered={isHovered} />
+//           <NavItem icon={<FaClipboardCheck size={20} />} text="Mis Aplicaciones" href="/estudiante/misaplicaciones" isHovered={isHovered} />
+//           <NavItem icon={<FaMailBulk  size={20} />} text="Notificaciones" href="/estudiante/notificaciones" isHovered={isHovered} />
+//           <button 
+//             className='hover:opacity-100 hover:w-[100%]'
+//             onClick={() => signOut()}
+//           >
+//           <NavItem icon={<FaExpeditedssl size={20} />} text="cerrar sesión"  href="/" isHovered={isHovered} />
+//           </button>
+          
+         
+//         </ul>
+//       </nav>
+//     </div>
+//   );
+// };
+
+// interface NavItemProps {
+//   icon: React.ReactNode;
+//   text: string;
+//   href: string;
+//   isHovered: boolean;
+// }
+
+// const NavItem: React.FC<NavItemProps> = ({ icon, text, href, isHovered }) => (
+//   <li>
+//     <Link href={href} className="flex items-center p-4 opacidadHover">
+//       <span className="mr-4">{icon}</span>
+//       <span className={`transition-all duration-300 ${isHovered ? 'opacity-100 w-auto' : 'opacity-0 w-0'} overflow-hidden whitespace-nowrap`}>
+//         {text}
+//       </span>
+//     </Link>
+//   </li>
+// );
+
+// export default NavBarEmergente;
+
+
+
+"use client";
 import React, { useState, useEffect } from 'react';
-import { FaTimes, FaHome, FaAddressCard, FaClipboardList, FaClipboardCheck, FaMailBulk, FaExpeditedssl } from 'react-icons/fa';
+import { FaTimes, FaHome, FaAddressCard, FaClipboardList, FaClipboardCheck, FaMailBulk, FaExpeditedssl, FaBuilding, FaUsers, FaUniversity } from 'react-icons/fa';
 import Link from 'next/link';
 import { useNavBar } from '@/context/NavBarContext';
 import { signOut, useSession } from "next-auth/react";
 
-
 const NavBarEmergente: React.FC = () => {
   const [isHovered, setIsHovered] = useState(false);
   const { isOpen, toggleNavBar } = useNavBar();
+  const { data: session, status } = useSession();
+  const [navigation, setNavigation] = useState<NavItemProps[]>([]);
+
+  const getLinks = () => {
+    if (status !== "authenticated") return;
+
+    if (session?.user.role === "estudiante") {
+      setNavigation([
+        { icon: <FaHome size={20} />, text: "Principal", href: "/estudiante", isHovered },
+        { icon: <FaAddressCard size={20} />, text: "Perfil", href: "/estudiante/perfil", isHovered },
+        { icon: <FaClipboardList size={20} />, text: "Ofertas de Vacante", href: "/estudiante/ofertas", isHovered },
+        { icon: <FaClipboardCheck size={20} />, text: "Mis Aplicaciones", href: "/estudiante/misaplicaciones", isHovered },
+        { icon: <FaMailBulk size={20} />, text: "Notificaciones", href: "/estudiante/notificaciones", isHovered },
+      ]);
+    } else if (session?.user.role === "alcaldia") {
+      setNavigation([
+        { icon: <FaHome size={20} />, text: "Principal", href: "/alcaldia", isHovered },
+        { icon: <FaAddressCard size={20} />, text: "Perfil", href: "/alcaldia/perfil", isHovered },
+        { icon: <FaClipboardList size={20} />, text: "Ofertas", href: "/alcaldia/ofertas", isHovered },
+        { icon: <FaMailBulk size={20} />, text: "Notificaciones", href: "/alcaldia/notificaciones", isHovered },
+        { icon: <FaUsers size={20} />, text: "Estudiantes", href: "/alcaldia/estudiantes", isHovered },
+        { icon: <FaBuilding size={20} />, text: "Instituciones", href: "/alcaldia/instituciones", isHovered },
+        { icon: <FaUniversity size={20} />, text: "Preregistros", href: "/alcaldia/preregister", isHovered },
+      ]);
+    } else if (session?.user.role === "dependencia") {
+      setNavigation([
+        { icon: <FaHome size={20} />, text: "Principal", href: "/dependencia", isHovered },
+        { icon: <FaAddressCard size={20} />, text: "Perfil", href: "/dependencia/perfil", isHovered },
+        { icon: <FaClipboardList size={20} />, text: "Mis Ofertas", href: "/dependencia/misofertas", isHovered },
+        { icon: <FaClipboardCheck size={20} />, text: "Crear Oferta", href: "/dependencia/misofertas/crearoferta", isHovered },
+        { icon: <FaMailBulk size={20} />, text: "Notificaciones", href: "/dependencia/notificaciones", isHovered },
+      ]);
+    }
+  };
+
+  useEffect(() => {
+    getLinks();
+  }, [status]);
 
   useEffect(() => {
     const handleEscape = (event: KeyboardEvent) => {
@@ -41,19 +162,28 @@ const NavBarEmergente: React.FC = () => {
       </button>
       <nav className="pt-16">
         <ul className="space-y-4">
-          <NavItem icon={<FaHome size={20} />} text="Principal" href="/estudiante" isHovered={isHovered} />
-          <NavItem icon={<FaAddressCard size={20} />} text="Perfil" href="/estudiante/perfil" isHovered={isHovered} />
-          <NavItem icon={<FaClipboardList size={20} />} text="Ofertas de Vacante" href="/estudiante/ofertas" isHovered={isHovered} />
-          <NavItem icon={<FaClipboardCheck size={20} />} text="Mis Aplicaciones" href="/estudiante/misaplicaciones" isHovered={isHovered} />
-          <NavItem icon={<FaMailBulk  size={20} />} text="Notificaciones" href="/estudiante/notificaciones" isHovered={isHovered} />
-          <button 
-            className='hover:opacity-100 hover:w-[100%]'
-            onClick={() => signOut()}
-          >
-          <NavItem icon={<FaExpeditedssl size={20} />} text="cerrar sesión"  href="/" isHovered={isHovered} />
-          </button>
-          
-         
+          {navigation.map((item) => (
+            <NavItem 
+              key={item.text}
+              icon={item.icon} 
+              text={item.text} 
+              href={item.href} 
+              isHovered={isHovered} 
+            />
+          ))}
+          {session && (
+            <button 
+              className="hover:opacity-100 hover:w-[100%]" 
+              onClick={() => signOut()}
+            >
+              <NavItem 
+                icon={<FaExpeditedssl size={20} />} 
+                text="Cerrar sesión" 
+                href="/" 
+                isHovered={isHovered} 
+              />
+            </button>
+          )}
         </ul>
       </nav>
     </div>
@@ -79,4 +209,3 @@ const NavItem: React.FC<NavItemProps> = ({ icon, text, href, isHovered }) => (
 );
 
 export default NavBarEmergente;
-
